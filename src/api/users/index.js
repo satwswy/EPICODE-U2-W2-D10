120a49afe1133833cb592949a8123aa818db1695
marios <mgeorgak859@gmail.com>
2022-09-09 13:48:48 +0300

    updates

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,21 +1,23 @@
-import express from "express"
-import createHttpError from "http-errors"
-import UsersModel from "./model.js"
+import express from "express";
+import createHttpError from "http-errors";
+import { adminOnlyMiddleware } from "../lib/auth/host.js";
+import { JWTAuthMiddleware } from "../lib/auth/token.js";
+import UsersModel from "./model.js";
 
-const usersRouter = express.Router()
+const usersRouter = express.Router();
 
 usersRouter.post("/", async (req, res, next) => {
-    try {
-      const newUser = new UsersModel(req.body) 
-      const { _id } = await newUser.save()
-  
-      res.status(201).send({ _id })
-    } catch (error) {
-      next(error)
-    }
-  })
+  try {
+    const newUser = new UsersModel(req.body);
+    const { _id } = await newUser.save();
 
-usersRouter.get("/", async (req, res, next) => {
+    res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
   try {
     const users = await UsersModel.find();
     res.send(users);
@@ -24,7 +26,46 @@ usersRouter.get("/", async (req, res, next) => {
   }
 });
 
-usersRouter.get("/:userId", async (req, res, next) => {
+usersRouter.get("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.user._id);
+    if (user) {
+      res.send(user);
+    } else {
+      next(createError(401, `User with id ${req.user._id} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.put("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const updatedUser = await UsersModel.findByIdAndUpdate(
+      req.user._id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (updatedUser) {
+      res.send(updatedUser);
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.delete("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    await UsersModel.findByIdAndDelete(req.user._id);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.get("/:userId", JWTAuthMiddleware, async (req, res, next) => {
   try {
     const user = await UsersModel.findById(req.params.userId);
     if (user) {
@@ -39,37 +80,68 @@ usersRouter.get("/:userId", async (req, res, next) => {
   }
 });
 
-usersRouter.put("/:userId", async (req, res, next) => {
-  try {
-    const updatedUser = await UsersModel.findByIdAndUpdate(
-      req.params.userId,
-      req.body,
-      { new: true, runValidators: true }
-    );
-
-    if (updatedUser) {
-      res.send(updatedUser);
-    } else {
-      next(
-        createHttpError(404, `User with id ${req.params.userId} not found!`)
+usersRouter.put(
+  "/:userId",
+  JWTAuthMiddleware,
+  adminOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      const updatedUser = await UsersModel.findByIdAndUpdate(
+        req.params.userId,
+        req.body,
+        { new: true, runValidators: true }
       );
-    }
-  } catch (error) {
-    next(error);
-  }
-});
 
-usersRouter.delete("/:userId", async (req, res, next) => {
-  try {
-    const deletedUser = await UsersModel.findByIdAndDelete(req.params.userId);
-    if (deletedUser) {
-      res.status(204).send();
-    } else {
-      next(
-        createHttpError(404, `User with id ${req.params.userId} not found!`)
-      );
+      if (updatedUser) {
+        res.send(updatedUser);
+      } else {
+        next(
+          createHttpError(404, `User with id ${req.params.userId} not found!`)
+        );
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
+
+usersRouter.delete(
+  "/:userId",
+  JWTAuthMiddleware,
+  adminOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      const deletedUser = await UsersModel.findByIdAndDelete(req.params.userId);
+      if (deletedUser) {
+        res.status(204).send();
+      } else {
+        next(
+          createHttpError(404, `User with id ${req.params.userId} not found!`)
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+usersRouter.post("/login", async (req, res, next) => {
+    try {
+      
+      const { email, password } = req.body
+  
+      
+      const user = await UsersModel.checkCredentials(email, password)
+  
+      if (user) {
+        
+        const token = await createAccessToken({ _id: user._id, role: user.role })
+        res.send({ accessToken: token })
+      } else {
+       
+        next(createError(401, "Credentials are not ok!"))
+      }
+    } catch (error) {
+      next(error)
+    }
+  })
